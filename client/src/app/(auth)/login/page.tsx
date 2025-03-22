f7e31655ba6440834ad0ad7f9fb4003157ef16f0
Zwea Htet <zhtet5@berkeley.edu>
2025-03-21 20:39:35 -0700

    refactor: simplify LoginPage component by removing unnecessary router logic

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,17 +1,10 @@
 // simply-learn/client/src/app/(auth)/login/page.tsx
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { LoginForm } from '@/components/Login';
+import { LoginForm } from "@/components/auth/login";
 
 export default function LoginPage() {
-    const router = useRouter();
-
-    return (
-        <div className='container mx-auto py-8 px-4'>
-            <LoginForm
-                onRegisterClick={() => router.push('/register')}
-            />
-        </div>
-    )
+	return (
+		<div className="container mx-auto py-8 px-4">
+			<LoginForm />
+		</div>
+	);
 }
