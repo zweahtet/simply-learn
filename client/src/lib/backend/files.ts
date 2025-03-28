64c7c601c5584c3f09ce5f67cf0af244af6f9552
Zwea Htet <zhtet5@berkeley.edu>
2025-03-28 12:00:51 -0700

    feat: implement file management functions with Supabase integration for fetching, deleting, and uploading files

---
+++ client/src/lib/backend/files.ts
@@ -0,0 +1,84 @@
+import { createClient } from "@/lib/supabase/client";
+import { SupabaseConfig } from "@/lib/defaults";
+import { FileMetadata } from "@/types";
+const supabase = createClient();
+
+/**
+ * Helper function to call the Supabase API to get the list of files in the bucket.
+ * @returns {Promise<FileMetadata[]>} - A promise that resolves to an array of file metadata objects.
+ */
+export async function getFiles(userId: string): Promise<Map<string, FileMetadata>> {
+    // list all folders under the userId directory in the bucket
+    const { data, error } = await supabase.storage
+        .from(SupabaseConfig.ATTACHMENT_BUCKET)
+        .list(userId);
+    
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    if (!data) return new Map<string, FileMetadata>();
+
+    // filter out .emptyFolderPlaceholder
+    const filteredData = data.filter(
+        (folder) => folder.name !== ".emptyFolderPlaceholder"
+    );
+
+    const fileMetadataMap = new Map<string, FileMetadata>();
+    for (const folder of filteredData) {
+        // get the content inside the folder
+        const { data, error } = await supabase.storage
+            .from(SupabaseConfig.ATTACHMENT_BUCKET)
+            .list(`${userId}/${folder.name}`);
+
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        if (!data) continue;
+
+        // there should be only one pdf file and we will use that to display the file
+        const pdfFile = data.find((file) => file.name.endsWith(".pdf"));
+        if (pdfFile) {
+            fileMetadataMap.set(folder.name, {
+                id: folder.name,
+                name: pdfFile.name,
+                type: pdfFile.metadata.mimetype,
+                size: pdfFile.metadata.size,
+            });
+        }
+    }
+    return fileMetadataMap;
+}
+
+/**
+ * Helper function to call the Supabase API to delete a file from the bucket.
+ */
+export async function deleteFile(userId: string, fileId: string) {
+    // list all files in the folder
+    const folderPath = `${userId}/${fileId}`;
+    const { data, error: listError } = await supabase.storage.from(SupabaseConfig.ATTACHMENT_BUCKET).list(folderPath);
+    
+    if (listError) {
+        throw new Error(listError.message);
+    }
+
+    if (!data) return;
+
+    // create array of file paths to delete
+    const filesToDelete = data.map((file) => `${folderPath}/${file.name}`);
+
+    // delete all files in the folder
+    const { error: deleteError } = await supabase.storage.from(SupabaseConfig.ATTACHMENT_BUCKET).remove(filesToDelete);
+
+    if (deleteError) {
+        throw new Error(deleteError.message);
+    }
+}
+
+/**
+ * Helper function to call the Supabase API to upload a file to the bucket.
+ */
+export async function uploadFile() {
+
+}
